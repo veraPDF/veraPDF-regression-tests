21b46c2bec1da369781e7ad813fc2e1af59e01a3
georgetarazevich <georgetarazevich@users.noreply.github.com>
2023-02-16 14:01:16 +0300

    Add new regression tests (#65)
    * Update regression tests

--- tools/test_helper/common-setup.bash
+++ tools/test_helper/common-setup.bash
@@ -7,3 +7,43 @@ _common_setup() {
     load "$PROJECT_ROOT/bats-support/load.bash"
     load "$PROJECT_ROOT/bats-assert/load.bash"
 }
+
+get_verapdf_version() {
+
+    currentIFS=$IFS
+    IFS=' '
+    veraPDF_info=$($PROJECT_ROOT/verapdf/verapdf --version | grep veraPDF)
+    read -r -a array <<<"$veraPDF_info"
+    VERAPDF_VERSION="${array[1]}"
+    IFS=$currentIFS
+
+    echo "$VERAPDF_VERSION"
+}
+
+verapdf_folder() {
+
+    currentIFS=$IFS
+    IFS='.'
+    folder_info=$(get_verapdf_version)
+    read -r -a array <<<"$folder_info"
+    FOLDER_VERSION="${array[0]}"."${array[1]}"
+    IFS=$currentIFS
+
+    echo "$FOLDER_VERSION"
+}
+
+get_plugin_version() {
+    PLUGIN_NAME=$1
+
+    currentIFS=$IFS
+    IFS='-'
+    plugin_info=$(cat "$PROJECT_ROOT/verapdf/config/plugins.xml" | grep "$PLUGIN_NAME")
+    read -r -a array <<<"$plugin_info"
+    version_info="${array[3]}"
+    IFS='.'
+    read -r -a array <<<"$version_info"
+    PLUGIN_VERSION="${array[0]}"."${array[1]}"."${array[2]}"
+    IFS=$currentIFS
+
+    echo "$PLUGIN_VERSION"
+}
